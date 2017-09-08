ebf7cbaa04f46e74c78482d5ce043acadb8e6105
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-09-08 08:35:11 -0700

    2

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import '../App.css';
+import router from '../router.js';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
       yup!
+      <Link to='/'>Home</Link>
+      <Link to='/users'>Users</Link>
+      { router }
       </div>
     );
   }
